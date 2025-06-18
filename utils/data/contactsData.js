2a1c894191b0c07a53494d0d4a1ef2ebae20c1ec
Kuber <kuberkhandare@gmail.com>
2025-06-18 11:16:49 +0530

    minor changes

--- utils/data/contactsData.js
+++
@@ -1,9 +0,0 @@
-export const contactsData = {
-    email: 'kuberkhandare@gmail.com',
-    phone: '0000000000',
-    address: 'Indian Institute of Information Technology, Lucknow, India',
-    github: 'https://github.com/KuberTheGreat',
-    linkedIn: 'https://www.linkedin.com/in/kuber-khandare-4b40ab27a/',
-    twitter: 'https://twitter.com/kuberkhandare',
-    devUsername: "kuberkhandare"
-}
